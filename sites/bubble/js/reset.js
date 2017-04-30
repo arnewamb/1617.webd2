ff3b76391d33da0b8472cc2e76912dbd23bde41f
arnewamb <arne.wambeke@student.arteveldehs.be>
2017-04-30 19:16:12 +0200

    message on sent

--- sites/bubble/js/reset.js
+++ sites/bubble/js/reset.js
@@ -1,13 +1,25 @@
+//copyright Arne Wambeke
+//29 april 2017
+
 //variabelen aanmaken
 var buttonReset = document.getElementById('reset');
 var inputField = document.getElementsByClassName('textField');
+//making new textnode
+var contactForm = document.getElementById('input');
+var node = document.createElement('p');
+var succesNode = document.createTextNode("Your message has been successfully sent");
+node.appendChild(succesNode);
 
 //function for reset
 buttonReset.onclick = function(){
+  //custom message
+contactForm.appendChild(succesNode);
+  //reset of value
   var waarde;
   for (var i = 0; i<6; i++){
   waarde = inputField[i].value = "";
-  console.log(waarde);
+  
   }
 };
+
 
